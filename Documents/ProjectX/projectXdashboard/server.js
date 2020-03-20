3523cd948e0624f67528aa558fb0bcac2fc872d1
Shivam Dixit <shivamdixit7077@gmail.com>
2020-03-20 15:50:15 +0530

    updates to deploy

--- Documents/ProjectX/projectXdashboard/server.js
+++ Documents/ProjectX/projectXdashboard/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/project-x-stocks-app'));
+app.use(express.static(__dirname + '/dist/dashboard'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/project-x-stocks-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/dashboard/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
